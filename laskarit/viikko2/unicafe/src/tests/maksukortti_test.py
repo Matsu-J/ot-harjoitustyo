5c6febce51e3ef69ed3ef0a99f0676cfc9b3f2bf
Matias Jerkkola <matsu.jerkkola@gmail.com>
2026-03-24 16:34:32 +0200

    week 2 exercise 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_rahan_lisaaminen_toimii_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 11.0)
+
+    def test_positiivinen_saldo_toimii_oikein(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 9.0)
+    
+    def test_rahaa_ei_voi_ottaa_liikaa(self):
+        self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_raha_riittaa_ja_voi_ottaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1000), True)
+    
+    def test_raha_ei_riita_ja_ei_voi_ottaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
